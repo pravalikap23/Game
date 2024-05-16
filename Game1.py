3934cc0acf7e43f8616f36a7a8e14bffb67bd1b5
pravalikap23 <saipravallikap58@gmail.com>
2024-05-16 14:45:33 +0100

    collision

--- Game1.py
+++ Game1.py
@@ -14,6 +14,20 @@ from pygame.locals import(
 pygame.init()
 pygame.font.init()
 
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super(Platform, self).__init__()
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        #self.rect = self.surf.get_rect()
+    
+    def update(self):
+        pygame.draw.rect(screen, (34, 139, 24), (self.x, self.y, self.width, self.height))
+        self.rect = self.surf.get_rect()
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
@@ -63,7 +77,6 @@ class Player(pygame.sprite.Sprite):
             if not self.rect.y <= self.jump_start_y:
                 self.fall_down = False
             
-
         if self.rect.left < 0:
             self.rect.left = 0
         elif self.rect.right > sw:
@@ -86,6 +99,16 @@ player = Player()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
+p1 = Platform(80, 300, 150, 100)
+p2 = Platform(290, 300, 150, 100)
+p3 = Platform(500, 300, 150, 100)
+p4 = Platform(700, 300, 150, 100)
+p5 = Platform(900, 300, 250, 100)
+
+all_sprites.add(p1, p2, p3, p4, p5)
+all_platforms = pygame.sprite.Group()
+all_platforms.add(p1, p2, p3, p4, p5)
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -100,7 +123,25 @@ while running:
     player.update(pressed_keys)
 
     screen.fill((173,216,230))
-    pygame.draw.rect(screen, (255, 0, 0), (35, sh-15, sw, 15), width = 0)
+    pygame.draw.rect(screen, (111, 78, 55), (0, sh-15, sw,15), width = 0)
+
+    # Lava
+    pygame.draw.rect(screen, (255, 0, 0), (45, sh-15, sw, 15), width = 0)
+    all_platforms.update()
+
+
+    if pygame.sprite.spritecollideany(player,all_platforms):
+        print("collision")
+
+    # if player.rect.colliderect((45, sh-15, sw, 15)):
+    #     player.rect.bottomleft = (0, sh-15)
+    
+    # Green rectangles
+    # pygame.draw.rect(screen, (34, 139, 24), (80, 300, 150, 100))
+    # pygame.draw.rect(screen, (34, 139, 24), (290, 300, 150, 100))
+    # pygame.draw.rect(screen, (34, 139, 24), (500, 300, 150, 100))
+    # pygame.draw.rect(screen, (34, 139, 24), (700, 300, 150, 100))
+    # pygame.draw.rect(screen, (34, 139, 24), (900, 300, 250, 100))
 
     for en in all_sprites:
         screen.blit(en.surf, en.rect)
